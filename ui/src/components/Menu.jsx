4214cf1f2059b1938a5e8322a222220305655610
gurramkshethra-droid <gurramkshethra@gmail.com>
2026-03-09 19:52:53 +0530

    Update Menu.jsx

--- ui/src/components/Menu.jsx
+++ ui/src/components/Menu.jsx
@@ -2,7 +2,7 @@ function Menu({ items }) {
     return (
         <div className="menu">
             {items.map((item, index) => (
-                <a key={index} href={`/${item}`}>{item.charAt(0).toUpperCase() + item.slice(1)}</a>
+                <a key={index} href={`/${item}`}>{item.charAt(0).toUpperCase() + item.slice(1)}</a>/*Menu itms*/
             ))}
         </div>
     )
